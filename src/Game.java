a19621735e480dc33fb0ce57c50d7ab9f9b85fba
Andrew Dutcher <andrew@andrewdutcher.com>
2015-08-02 19:08:21 -0700

    Switch space indents to tabs

--- src/Game.java
+++ src/Game.java
@@ -8,48 +8,48 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Game extends JPanel {
-    public static final String NAME = "Titanium Gastropod v0.0";
-    public static final int WINDOW_W = 800, WINDOW_H = 600;
+	public static final String NAME = "Titanium Gastropod v0.0";
+	public static final int WINDOW_W = 800, WINDOW_H = 600;
 
-    private BufferedImage mScreenBuffer;
+	private BufferedImage mScreenBuffer;
 
-    public Game() {
-	mScreenBuffer = new BufferedImage(WINDOW_W, WINDOW_H, BufferedImage.TYPE_INT_ARGB);
-	
-	setFocusable(true);
-	setPreferredSize(new Dimension(WINDOW_W, WINDOW_H));
+	public Game() {
+		mScreenBuffer = new BufferedImage(WINDOW_W, WINDOW_H, BufferedImage.TYPE_INT_ARGB);
+		
+		setFocusable(true);
+		setPreferredSize(new Dimension(WINDOW_W, WINDOW_H));
 
-	Thread updateThread = new Thread("Game update thread") {
-		@Override
-		public void run() {
-		    while (true) {
-			Graphics2D g = (Graphics2D) mScreenBuffer.getGraphics();
-			g.setColor(Color.BLACK);
-			g.fillRect(0, 0, WINDOW_W, WINDOW_H);
+		Thread updateThread = new Thread("Game update thread") {
+			@Override
+			public void run() {
+				while (true) {
+				Graphics2D g = (Graphics2D) mScreenBuffer.getGraphics();
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 0, WINDOW_W, WINDOW_H);
 
-			// do game drawing here
+				// do game drawing here
 
-			repaint();
-			try {
-			    Thread.sleep(30);
-			} catch (InterruptedException e) {
-			    e.printStackTrace();
+				repaint();
+				try {
+					Thread.sleep(30);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				}
 			}
-		    }
-		}
-	    };
-	updateThread.start();
-    }
+			};
+		updateThread.start();
+	}
 
-    public void paint(Graphics g) {
-	g.drawImage(mScreenBuffer, 0, 0, this);
-    }
-    
-    public static void main(String args[]) {
-	final JFrame frame = new JFrame(NAME);
-	frame.getContentPane().add(new Game());
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.pack();
-	frame.setVisible(true);
-    }
+	public void paint(Graphics g) {
+		g.drawImage(mScreenBuffer, 0, 0, this);
+	}
+	
+	public static void main(String args[]) {
+		final JFrame frame = new JFrame(NAME);
+		frame.getContentPane().add(new Game());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
