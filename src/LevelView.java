80d713ba1edc7f18d5cd57ffdf9dd86b13b94d52
Timeroot <timeroot.alex@gmail.com>
2015-08-05 15:32:01 -0700

    ImageLoader added
    Supports jar loading and directory loading. All pngs placed in res will
    work. Makefile added so that they get packaged in as well.

--- src/LevelView.java
+++ src/LevelView.java
@@ -17,7 +17,7 @@ public class LevelView extends View {
     	mWorld = new World(GRAVITY);
 	
 		{ //Create wall
-			BodyDef bodyDef = new BodyDef();
+			BodyDef bodyDef = new BodyDef();//
 			bodyDef.type = BodyType.STATIC;
 			bodyDef.position.set(0, 0);
 			bodyDef.angle = 0f;
@@ -27,7 +27,7 @@ public class LevelView extends View {
 			shape.setAsBox(300, 10, new Vec2(150,5), 0);
 			
 			FixtureDef fdef = new FixtureDef();
-			fdef.friction = 1f;
+			fdef.friction = 0.6f;
 			fdef.shape = shape;
 			shape.setAsBox(300, 10, new Vec2(300,10), 0);
 			wall.createFixture(fdef);
@@ -45,81 +45,75 @@ public class LevelView extends View {
 
 			FixtureDef fdef = new FixtureDef();
 			fdef.shape = shape;
-			fdef.friction = 1f;
+			fdef.friction = 0.6f;
 
+//			ConstantVolumeJointDef cvjd = new ConstantVolumeJointDef();
+//		    cvjd.frequencyHz = 10.0f;
+//		    cvjd.dampingRatio = 10.0f;
+//		    jarvis = new Body[jCount];
+//			//See http://shiffman.net/itp/classes/nature/box2d_2010/blob/Blob.pde for blob
+//			for(int i=0;i<jCount;i++){ 
+//				wall = mWorld.createBody(bodyDef);
+//				bodyDef.position.set(50+10f*(float)Math.cos(i*Math.PI*2/jCount), 50+10f*(float)Math.sin(i*Math.PI*2/jCount));
+//				
+//				jarvis[i] = mWorld.createBody(bodyDef);
+//				jarvis[i].createFixture(fdef);
+//				
+//				cvjd.addBody(jarvis[i]);
+//				
+//			}
+//			ConstantVolumeJoint cvj = (ConstantVolumeJoint) mWorld.createJoint(cvjd);
 			
-		    
-			/*
-			ConstantVolumeJointDef cvjd = new ConstantVolumeJointDef();
-
-		    cvjd.frequencyHz = 10.0f;
-		    cvjd.dampingRatio = 10.0f;
-		    jarvis = new Body[jCount];
-			//See http://shiffman.net/itp/classes/nature/box2d_2010/blob/Blob.pde for blob
-			for(int i=0;i<jCount;i++){ 
-				wall = mWorld.createBody(bodyDef);
-				bodyDef.position.set(50+10f*(float)Math.cos(i*Math.PI*2/jCount), 50+10f*(float)Math.sin(i*Math.PI*2/jCount));
-				
-				jarvis[i] = mWorld.createBody(bodyDef);
-				jarvis[i].createFixture(fdef);
-				
-				cvjd.addBody(jarvis[i]);
-				
-			}
-			
-			ConstantVolumeJoint cvj = (ConstantVolumeJoint) mWorld.createJoint(cvjd);
-			*/
 			
 			jarvis = new Body[jCount][jCount];
 			for(int i=0;i<jCount;i++){
-				for(int j=0;j<=i;j++){
+				for(int j=0;j<jCount;j++){
+//				for(int j=0;j<=i;j++){
 					wall = mWorld.createBody(bodyDef);
-//					bodyDef.position.set(300+i*6, 300+j*6);
-					
-					bodyDef.position.set(300+3*i*(float)Math.cos(0.3*j*Math.PI*2/(i+0.3)+1), 300+3*i*(float)Math.sin(0.3*j*Math.PI*2/(i+0.3)+1));
-					
+					bodyDef.position.set(300+i*6, 300+j*6);
+//					bodyDef.position.set(300+3*i*(float)Math.cos(0.3*j*Math.PI*2/(i+0.3)+1), 300+3*i*(float)Math.sin(0.3*j*Math.PI*2/(i+0.3)+1));
 					jarvis[i][j] = mWorld.createBody(bodyDef);
 					jarvis[i][j].createFixture(fdef);
 				}
 			}
 			
-//			for(int i=0;i<jCount-2;i++){
-//				for(int j=0;j<jCount-2;j++){
-//					ConstantVolumeJointDef cvjd = new ConstantVolumeJointDef();
-//
-//				    cvjd.frequencyHz = 10.0f;
-//				    cvjd.dampingRatio = 10.0f;
-//				    
-//				    cvjd.addBody(jarvis[i][j]);
-//				    cvjd.addBody(jarvis[i+1][j]);
-//				    cvjd.addBody(jarvis[i+2][j]);
-//				    cvjd.addBody(jarvis[i+2][j+1]);
-//				    cvjd.addBody(jarvis[i+2][j+2]);
-//				    cvjd.addBody(jarvis[i+1][j+2]);
-//				    cvjd.addBody(jarvis[i][j+2]);
-//				    cvjd.addBody(jarvis[i][j+1]);
-//				    
-//					ConstantVolumeJoint cvj = (ConstantVolumeJoint) mWorld.createJoint(cvjd);
-//				}
-//			}
-			
 			for(int i=0;i<jCount-2;i++){
-				for(int j=0;j<=i;j++){
+				for(int j=0;j<jCount-2;j++){
 					ConstantVolumeJointDef cvjd = new ConstantVolumeJointDef();
 
-				    cvjd.frequencyHz = 4.0f;
+				    cvjd.frequencyHz = 10.0f;
 				    cvjd.dampingRatio = 10.0f;
 				    
 				    cvjd.addBody(jarvis[i][j]);
-				    cvjd.addBody(jarvis[i+1][j+1]);
-				    cvjd.addBody(jarvis[i+2][j+2]);
-				    cvjd.addBody(jarvis[i+2][j+1]);
-				    cvjd.addBody(jarvis[i+2][j]);
 				    cvjd.addBody(jarvis[i+1][j]);
+				    cvjd.addBody(jarvis[i+2][j]);
+				    cvjd.addBody(jarvis[i+2][j+1]);
+				    cvjd.addBody(jarvis[i+2][j+2]);
+				    cvjd.addBody(jarvis[i+1][j+2]);
+				    cvjd.addBody(jarvis[i][j+2]);
+				    cvjd.addBody(jarvis[i][j+1]);
 				    
 					ConstantVolumeJoint cvj = (ConstantVolumeJoint) mWorld.createJoint(cvjd);
 				}
 			}
+			
+//			for(int i=0;i<jCount-2;i++){
+//				for(int j=0;j<=i;j++){
+//					ConstantVolumeJointDef cvjd = new ConstantVolumeJointDef();
+//
+//				    cvjd.frequencyHz = 4.0f;
+//				    cvjd.dampingRatio = 10.0f;
+//				    
+//				    cvjd.addBody(jarvis[i][j]);
+//				    cvjd.addBody(jarvis[i+1][j+1]);
+//				    cvjd.addBody(jarvis[i+2][j+2]);
+//				    cvjd.addBody(jarvis[i+2][j+1]);
+//				    cvjd.addBody(jarvis[i+2][j]);
+//				    cvjd.addBody(jarvis[i+1][j]);
+//				    
+//					ConstantVolumeJoint cvj = (ConstantVolumeJoint) mWorld.createJoint(cvjd);
+//				}
+//			}
 		
 		}
 	
@@ -132,12 +126,12 @@ public class LevelView extends View {
     	if(k.isPressed(KeyType.LEFT)){
     		for(Body[] aB:jarvis)
     			for(Body b:aB)
-    				if(b!=null) b.applyForceToCenter(new Vec2(-1000,0));
+    				if(b!=null) b.applyForceToCenter(new Vec2(-100,0));
 //    		jarvis[0][0].applyForceToCenter(new Vec2(-1000,0));
     	} else if (k.isPressed(KeyType.RIGHT)){
     		for(Body[] aB:jarvis)
     			for(Body b:aB)
-    				if(b!=null) b.applyForceToCenter(new Vec2(1000,0));
+    				if(b!=null) b.applyForceToCenter(new Vec2(100,0));
 //    		jarvis[0][0].applyForceToCenter(new Vec2(1000,0));
     	}
     }
